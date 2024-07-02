6b5868578bf99f2da4307708c36210f782ebf0ad
abdoomr2003 <abdoomer1112003@gmail.com>
2024-07-02 19:36:01 +0300

    fifth task

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""
+This module starts a Flask web application and renders a template on the root
+route.
+
+The application runs on host '0.0.0.0' and port 5000 with debug mode enabled.
+"""
+
+from flask import Flask, render_template
+from flask_babel import Babel, request
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """
+    Configuration class for setting application parameters.
+
+    Attributes:
+        LANGUAGES (list): Supported languages.
+        BABEL_DEFAULT_LOCALE (str): Default locale for the application.
+        BABEL_DEFAULT_TIMEZONE (str): Default timezone for the application.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """
+    Selects the best match for the client's preferred language.
+
+    This function is used by Flask-Babel to determine which language to use
+    for translations. It checks the languages preferred by the client (as
+    indicated by the 'Accept-Language' header in the request) and matches
+    them against the supported languages configured in the application.
+
+    Returns:
+        str: The best matching language code from the supported languages,
+        or the default language if no match is found.
+    """
+    local = request.args.get('locale')
+    if local and local in app.config['LANGUAGES']:
+        return local
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def hello_world() -> str:
+    """
+    Renders the '3-index.html' template on the root route.
+
+    Returns:
+        str: The rendered HTML content of the '1-index.html' template.
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    """
+    Starts the Flask web application.
+
+    The application will run in debug mode, listening on all available
+    IP addresses on port 5000.
+    """
+    app.run(debug=True, host='0.0.0.0', port=5000)
